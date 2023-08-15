1280d4e78d11daf010699d790c9aefe4176220d6
Brito <jmbmaluno@outlook.com>
2023-08-15 14:07:41 -0300

    Adicionado calculo para 3 ramos

--- nimber_arvore.py
+++ nimber_arvore.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+'''
+PARA ÁRVORES COM 2 RAMOS
+
+
 def calcular_nim(x,y):
     valores = []
     
@@ -33,4 +38,39 @@ for x in range(n):
 df = pd.DataFrame(resultado)
 
 df.to_excel("resultado.xlsx", index = False)
+'''
 
+####################################################################
+
+
+#PARA ÁRVORES COM 3 RAMOS
+def calcular_nim(x,y,z):
+    valores = []
+
+    for i in range(x+1):
+        valores.append(i^y^z)
+    
+    for j in range(y+1):
+        valores.append(x^j^z)
+    
+    for k in range(z+1):
+        valores.append(x^y^k)
+
+    
+    return mex(valores)
+
+n = 4
+
+qtde_ramos = []
+resultado = []
+
+for x in range(n):
+    for y in range(n):
+        for z in range(n):
+                if(x <= y and y <= z):
+                     qtde_ramos.append((x+1,y+1,z+1))
+                     resultado.append(calcular_nim(x+1,y+1,z+1))
+
+
+for i in range(len(qtde_ramos)):
+    print("Para ", qtde_ramos[i], ": ", resultado[i])
